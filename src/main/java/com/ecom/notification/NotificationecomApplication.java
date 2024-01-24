c6fe98bb80125a83fa0a08d9d951bf014d443320
shubham <shubhamgogawale1@gmail.com>
2024-01-24 13:22:26 +0530

    first commit

---
+++ src/main/java/com/ecom/notification/NotificationecomApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationecomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationecomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/notification/NotificationecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationecomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
